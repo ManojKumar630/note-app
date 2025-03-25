5eb3e49eb10c87964945fa5ffc310585095e30b2
Manoj Kumar <manojkumarkondaparthii@gmail.com>
2025-03-25 12:29:51 +0530

    Updates

---
+++ precache-manifest.be075a15a15af860645330653a8fe153.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7225fe2362507929ac45eb47eba1f1be",
+    "url": "/note-app/index.html"
+  },
+  {
+    "revision": "638e200e69d950262938",
+    "url": "/note-app/static/js/2.ae026ab9.chunk.js"
+  },
+  {
+    "revision": "8b7ff06827318dc3739f",
+    "url": "/note-app/static/js/main.88216226.chunk.js"
+  },
+  {
+    "revision": "47b98c4c94c3392872d3",
+    "url": "/note-app/static/js/runtime~main.a60a26f2.js"
+  }
+]);
